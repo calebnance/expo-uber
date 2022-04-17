4df738e887af77487605298f48edaae3925f0761
calebnance <calebnance@gmail.com>
2022-04-17 00:10:37 -0400

    small updates

--- App.js
+++ App.js
@@ -3,39 +3,32 @@ import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { func } from './src/constants';
 
-// main navigation stack
+// root navigation stack
 import RootStack from './src/navigation/RootStack';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
 
-    this.state = {
-      isLoading: true
-    };
-  }
-
-  render() {
-    const { isLoading } = this.state;
-
-    if (isLoading) {
-      return (
-        <AppLoading
-          onError={() => {
-            // console.warn
-          }}
-          onFinish={() => this.setState({ isLoading: false })}
-          startAsync={func.loadAssetsAsync}
-        />
-      );
-    }
-
+  // pre-loading assets/fonts?
+  if (isLoading) {
     return (
-      <React.Fragment>
-        <StatusBar barStyle="dark-content" />
-
-        <RootStack />
-      </React.Fragment>
+      <AppLoading
+        onError={() => {
+          // console.warn
+        }}
+        onFinish={() => setIsLoading(false)}
+        startAsync={func.loadAssetsAsync}
+      />
     );
   }
-}
+
+  return (
+    <React.Fragment>
+      <StatusBar barStyle="dark-content" />
+
+      <RootStack />
+    </React.Fragment>
+  );
+};
+
+export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -32,148 +32,126 @@ const types = {
   }
 };
 
-class Home extends React.Component {
-  constructor() {
-    super();
+const Home = ({ navigation }) => {
+  const [type, setType] = React.useState('car');
+  const [selectType, setSelectType] = React.useState(false);
+  const [showMap, setShowMap] = React.useState(false);
+  const [coordinates, setCoords] = React.useState({ lat: null, lon: null });
 
-    this.state = {
-      type: 'car',
-      selectType: false,
-      showMap: false,
-      userLat: null,
-      userLon: null
+  React.useEffect(() => {
+    const getLocation = async () => {
+      // get exisiting locaton permissions first
+      const { status: existingStatus } =
+        await Location.requestForegroundPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      // ask again to grant locaton permissions (if not already allowed)
+      if (existingStatus !== 'granted') {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        finalStatus = status;
+      }
+
+      // still not allowed to use location?
+      if (finalStatus !== 'granted') {
+        return;
+      }
+
+      const { coords } = await Location.getCurrentPositionAsync();
+
+      setCoords({ lat: coords.latitude, lon: coords.longitude });
+      setShowMap(true);
     };
 
-    this.toggleTypeModal = this.toggleTypeModal.bind(this);
-    this.changeRideType = this.changeRideType.bind(this);
-  }
+    getLocation().catch(console.error);
+  }, []);
 
-  async componentDidMount() {
-    // get exisiting locaton permissions first
-    const { status: existingStatus } =
-      await Location.requestForegroundPermissionsAsync();
-    let finalStatus = existingStatus;
+  const toggleTypeModal = () => {
+    setSelectType(!selectType);
+  };
 
-    // ask again to grant locaton permissions (if not already allowed)
-    if (existingStatus !== 'granted') {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      finalStatus = status;
-    }
+  return (
+    <View style={gStyle.container}>
+      {showMap && (
+        <MapView
+          followsUserLocation
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: coordinates.lat,
+            longitude: coordinates.lon,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+          }}
+          showsUserLocation
+          style={styles.map}
+        />
+      )}
 
-    // still not allowed to use location?
-    if (finalStatus !== 'granted') {
-      return;
-    }
-
-    const { coords } = await Location.getCurrentPositionAsync();
-
-    this.setState({
-      showMap: true,
-      userLat: coords.latitude,
-      userLon: coords.longitude
-    });
-  }
-
-  toggleTypeModal() {
-    this.setState((prevState) => ({
-      selectType: !prevState.selectType
-    }));
-  }
-
-  changeRideType(type) {
-    this.setState({
-      type
-    });
-  }
-
-  render() {
-    const { navigation } = this.props;
-    const { type, selectType, showMap, userLat, userLon } = this.state;
-
-    return (
-      <View style={gStyle.container}>
-        {showMap && (
-          <MapView
-            followsUserLocation
-            provider={PROVIDER_GOOGLE}
-            region={{
-              latitude: userLat,
-              longitude: userLon,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01
-            }}
-            showsUserLocation
-            style={styles.map}
+      {!showMap && (
+        <View style={styles.containerNoLocation}>
+          <Text style={styles.textLocationNeeded}>
+            We need your location data...
+          </Text>
+          <TouchText
+            onPress={() => Linking.openURL('app-settings:')}
+            style={styles.btnGoTo}
+            styleText={styles.btnGoToText}
+            text="Go To Permissions"
           />
-        )}
-
-        {!showMap && (
-          <View style={styles.containerNoLocation}>
-            <Text style={styles.textLocationNeeded}>
-              We need your location data...
-            </Text>
-            <TouchText
-              onPress={() => Linking.openURL('app-settings:')}
-              style={styles.btnGoTo}
-              styleText={styles.btnGoToText}
-              text="Go To Permissions"
-            />
-          </View>
-        )}
-
-        {type === 'bike' && (
-          <View style={styles.rightContainer}>
-            <View style={styles.icons}>
-              <TouchIcon
-                icon={<SvgQRCode />}
-                iconSize={20}
-                onPress={() => navigation.navigate('ModalQRCode')}
-                style={[styles.icon, styles.iconQRCode]}
-              />
-              <TouchIcon
-                icon={<SvgCheckShield />}
-                iconSize={20}
-                onPress={() => navigation.navigate('ModalTutorialBike')}
-                style={[styles.icon, styles.iconShield]}
-              />
-            </View>
-          </View>
-        )}
-
-        <View style={styles.header}>
-          <TouchIcon
-            icon={<SvgMenu />}
-            iconSize={32}
-            onPress={() => navigation.toggleDrawer()}
-          />
-          <RequestRideType
-            image={types[type].image}
-            onPress={this.toggleTypeModal}
-            text={types[type].text}
-          />
-
-          {type === 'car' && <View style={styles.placeholder} />}
-          {type === 'bike' && (
-            <TouchText
-              onPress={() => navigation.navigate('ModalHelp')}
-              style={styles.help}
-              text="Help"
-            />
-          )}
         </View>
+      )}
 
-        <SelectRideType
-          data={types}
-          onClose={this.toggleTypeModal}
-          onSelect={this.changeRideType}
-          visible={selectType}
+      {type === 'bike' && (
+        <View style={styles.rightContainer}>
+          <View style={styles.icons}>
+            <TouchIcon
+              icon={<SvgQRCode />}
+              iconSize={20}
+              onPress={() => navigation.navigate('ModalQRCode')}
+              style={[styles.icon, styles.iconQRCode]}
+            />
+            <TouchIcon
+              icon={<SvgCheckShield />}
+              iconSize={20}
+              onPress={() => navigation.navigate('ModalTutorialBike')}
+              style={[styles.icon, styles.iconShield]}
+            />
+          </View>
+        </View>
+      )}
+
+      <View style={styles.header}>
+        <TouchIcon
+          icon={<SvgMenu />}
+          iconSize={32}
+          onPress={() => navigation.toggleDrawer()}
+        />
+        <RequestRideType
+          image={types[type].image}
+          onPress={toggleTypeModal}
+          text={types[type].text}
         />
 
-        {type === 'car' && <WhereTo />}
+        {type === 'car' && <View style={styles.placeholder} />}
+        {type === 'bike' && (
+          <TouchText
+            onPress={() => navigation.navigate('ModalHelp')}
+            style={styles.help}
+            text="Help"
+          />
+        )}
       </View>
-    );
-  }
-}
+
+      <SelectRideType
+        data={types}
+        onClose={toggleTypeModal}
+        onSelect={(selectedType) => setType(selectedType)}
+        visible={selectType}
+      />
+
+      {type === 'car' && <WhereTo />}
+    </View>
+  );
+};
 
 Home.propTypes = {
   // required
